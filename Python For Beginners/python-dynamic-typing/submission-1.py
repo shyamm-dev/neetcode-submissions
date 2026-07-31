1e3b3453656c9bfb65746108e9b79569e313f696
Shyamm Sundar K <shyammsundar.k.dev@gmail.com>
2026-08-01 04:41:23 +0530

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = ""
+print(type(variable))
+variable = []
+print(type(variable))
