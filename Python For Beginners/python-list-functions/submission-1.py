232c200d2a4d228f0fb0a82dbd21b160197eacac
Shyamm Sundar K <shyammsundar.k.dev@gmail.com>
2026-08-01 21:33:47 +0530

    Add: python-list-functions - submission-1

---
+++ Python For Beginners/python-list-functions/submission-1.py
@@ -0,0 +1,29 @@
+from typing import List # this is used to add type hints for List type
+
+def get_sum(nums: List[int]) -> int:
+    s = 0
+    for i in nums:
+        s += i
+    return s
+
+def get_min(nums: List[int]) -> int:
+    m = float("inf")
+    for i in nums:
+        m = m if m < i else i
+    return m
+
+def get_max(nums: List[int]) -> int:
+    m = float("-inf")
+    for i in nums:
+        m = m if m > i else i
+    return m
+
+# do not modify below this line
+print(get_sum([1, 2, 3, 4, 5]))
+print(get_sum([5, 4, 5, 6]))
+
+print(get_min([7, 3, 4, 5]))
+print(get_min([5, 4, 5, 6]))
+
+print(get_max([7, 3, 4, 5]))
+print(get_max([5, 4, 5, 6]))
