6019ac595d3b06b859cdd776f4612bb589ef543a
Shyamm Sundar K <shyammsundar.k.dev@gmail.com>
2026-08-09 20:26:22 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hash_map = defaultdict(int)
+
+        for i in s:
+            hash_map[i] += 1
+        
+        for i in t:
+            hash_map[i] -= 1
+        
+        return not max(hash_map.values())
